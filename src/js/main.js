e06ccb0da5b4a5985883cb6d7c3e9ec059ba5a4d
Harimad <leesh68481@gmail.com>
2022-04-07 14:27:23 +0900

    Handle scrolling when tapping on the navbar menu

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,29 @@ document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) $('#navbar').classList.add('navbar--dark')
   else $('#navbar').classList.remove('navbar--dark')
 })
+
+// Handle scrolling when tapping on the navbar menu
+$('.navbar__menu').addEventListener('click', e => {
+  const dataSetLink = e.target.dataset.link
+  if (dataSetLink) {
+    $(`${dataSetLink}`).scrollIntoView({ behavior: 'smooth' })
+  }
+})
+
+$('.home__contact').addEventListener('click', e => {
+  $(`#contact`).scrollIntoView({ behavior: 'smooth' })
+})
+
+// Navbar toggle button for small screen
+
+// Handle click on "contact me" button on home
+
+// Make home slowly fade to transparent as the window scrolls down
+
+// Show "arrow up" button when scrolling down
+
+// Handle click on the "arrow up" button
+
+// Projects
+
+// Remove selection from the previous item and select the new one
